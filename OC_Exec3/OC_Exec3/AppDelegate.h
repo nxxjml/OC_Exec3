078aa5e26d2109317d36938ea7aab1ef6d45cbea
nxxjml <nxxjml@sina.cn>
2015-10-11 04:52:02 -0700

    project init

---
+++ OC_Exec3/OC_Exec3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_Exec3
+//
+//  Created by simao on 15/10/11.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC_Exec3/OC_Exec3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OC_Exec3
+//
+//  Created by simao on 15/10/11.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
